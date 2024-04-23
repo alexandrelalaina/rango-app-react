a4f03951de73a2a617e1092f26240a68ce7fab37
Alexandre La Laina <alexandre_lalaina@yahoo.com.br>
2024-04-22 23:47:55 -0300

    14/04/24 Criacao header component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import Header from "./componentes/Header/Header";
 import Footer from "./componentes/Footer/Footer";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
+import NotFound from "./pages/404/NotFound";
 
 function App() {
   return (
           <>
               <Header />
-              <h2>Rango em Construcao...</h2>
               <Routes>
-              <Route path="/" element={<Home />} />
+                <Route path="/" element={<Home />} />
+                <Route path="*" element={<NotFound />} />
               </Routes>
               <Footer />
           </>
